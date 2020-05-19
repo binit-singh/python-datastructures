1047c9b6017cb2feecb0fba43add37d86b03eece
binit-singh <binit4you@gmail.com>
2020-05-20 00:55:31 +0530

    Add boilerplate code to call calc_fib
    Signed-off-by: binit-singh <binit4you@gmail.com>

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,17 @@
 # Uses python3
+import time
+
+
 def calc_fib(n):
-    if (n <= 1):
+    if n <= 1:
         return n
 
     return calc_fib(n - 1) + calc_fib(n - 2)
 
-n = int(input())
-print(calc_fib(n))
+
+if __name__ == '__main__':
+    n = int(input())
+    a = time.time()
+    print(calc_fib(n))
+    b = time.time()
+    print('Time taken:', (b-a))
